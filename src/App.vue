f99e8144fac823c95bcb8d6545bfb9d379a613a0
Anjiu <68489044@qq.com>
2023-07-08 17:52:18 +0800

    feat: src别名配置

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>app</div>
+  <div>
+    app
+    <el-button type="primary" size="default" :icon="Edit">默认按钮</el-button>
+
+    <el-pagination
+      :page-size="100"
+      layout="total, prev, pager, next"
+      :total="1000"
+    ></el-pagination>
+  </div>
 </template>
 
-<script setup></script>
+<script lang="ts" setup>
+import { Edit } from '@element-plus/icons-vue'
+</script>
 
 <style lang="" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve('./src'), // 相对路径别名配置，使用 @ 代替 src
+    },
+  },
 })
